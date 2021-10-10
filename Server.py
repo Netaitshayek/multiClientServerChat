ca008060e73dd547a9ab6145c95fa3d75d90f89f
netait <netaitshayek@gmail.com>
2021-10-10 21:36:46 +0300

    Added first cr changes

--- Server.py
+++ Server.py
@@ -1,19 +1,21 @@
 import socket
 import select
-from datetime import date, datetime, timedelta
+from datetime import datetime
 
 # 12.6 Server Neta Itshayek
-
+# TODO: move to constant file
 MAX_MSG_LENGTH = 1024
 SERVER_PORT = 5555
 SERVER_IP = "0.0.0.0"
 
 
 def print_client_sockets(client_sockets):
+    # TODO: Replace with "\t".join(client_sockets)
     for c in client_sockets:
         print("\t", c.getpeername())
 
-
+# TODO: TIME NOW Is a know parameter ( datetime.now )
+# TODO: all this can be replaced with datetime.strftime()
 def return_time(time_now):
     minute = "0"
     if time_now.minute < 10:
@@ -27,40 +29,53 @@ def return_time(time_now):
 
 def main():
     print("Setting up server...")
+    # TODO: move to Server class and put this in ctor
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((SERVER_IP, SERVER_PORT))
     server_socket.listen()
+
     print("Listening for clients...")
     client_sockets = []
     messages_to_send = []
     managers = []
 
     while True:
+        # TODO: naming ????
         rlist, wlist, xlist = select.select([server_socket] + client_sockets, client_sockets, [])
         for current_socket in rlist:
+            # TODO: Move to function
             if current_socket is server_socket:
-                connection, client_address = current_socket.accept()
                 print("New client joined", client_address)
+                connection, client_address = current_socket.accept()
                 client_sockets.append(connection)
                 print_client_sockets(client_sockets)
             else:
+                # TODO: magic floaty number ( replace with const or action dictionary )
                 name_length = current_socket.recv(4).decode()
                 name = current_socket.recv(int(name_length)).decode()
+                # TODO: magic floaty number ( replace with const or action dictionary )
                 command = current_socket.recv(1).decode()
+                # TODO: magic floaty number ( replace with const or action dictionary )
                 request_length = current_socket.recv(4).decode()
                 request = current_socket.recv(int(request_length)).decode()
 
+                # TODO: magic floaty number ( replace with const or action dictionary )
                 if command == "5":
+                    # TODO: magic floaty number ( replace with const or action dictionary )
                     private_message_length = current_socket.recv(4).decode()
                     private_message = current_socket.recv(int(private_message_length)).decode()
 
+                # TODO: Replace Neta with const
                 if name == "@Neta" and name not in managers:
                     managers.append("@Neta")
 
+                # TODO: why? this is only used once no reason to cast it to variable
                 current_time = datetime.now()
                 new_time = return_time(current_time)
+                # TODO: use Neta const
                 if request == "@Neta is a manager now":
                     request_length = str(len(request))
+                    # TODO: magic floaty number ( replace with const or action dictionary )
                     zfill_length = request_length.zfill(4)
                     request = zfill_length + request
                     for new_other_current_socket2 in wlist:
@@ -81,6 +96,7 @@ def main():
                                 managers.remove(name)
                             request = new_time + " " + name + " has left the chat!"
                             request_length = str(len(request))
+                            # TODO: magic floaty number ( replace with const or action dictionary )
                             zfill_length = request_length.zfill(4)
                             request = zfill_length + request
                             for new_other_current_socket in wlist:
@@ -92,23 +108,29 @@ def main():
                             print_client_sockets(client_sockets)
 
                         else:
+                            # TODO: replace with switch case dictionary
+                            # TODO: magic floaty number ( replace with const or action dictionary )
                             if command == "1":
                                 request = new_time + " " + name + ": " + request + "1"
                                 print(request)
                                 messages_to_send.append((current_socket, request))
+                            # TODO: magic floaty number ( replace with const or action dictionary )
                             if command == "2" and name in managers and request not in managers:
                                 managers.append("@" + request)
                                 request = new_time + " " + "@" + request + " is a manager now" + "2"
                                 print(request)
                                 messages_to_send.append((current_socket, request))
+                            # TODO: magic floaty number ( replace with const or action dictionary )
                             if command == "3" and name in managers:
                                 request = new_time + " " + request + " has been kicked from the chat!" + "3"
                                 print(request)
                                 messages_to_send.append((current_socket, request))
+                            # TODO: magic floaty number ( replace with const or action dictionary )
                             if command == "4" and name in managers:
                                 request = new_time + " " + request + " cannot speak here" + "4"
                                 print(request)
                                 messages_to_send.append((current_socket, request))
+                            # TODO: magic floaty number ( replace with const or action dictionary )
                             if command == "5":
                                 request = request + "$" + new_time + " !" + name + ": " + private_message + "5"
                                 print(request)
